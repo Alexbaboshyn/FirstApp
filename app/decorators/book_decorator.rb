59b95a8eabded6248f5c94c9c592a29a3582e1bc
Alexbaboshyn <Alexbaboshyn@gmail.com>
2017-08-04 16:54:54 +0300

    book decorator

--- app/decorators/book_decorator.rb
+++ app/decorators/book_decorator.rb
@@ -1,9 +1,26 @@
 class BookDecorator < Draper::Decorator
   delegate_all
+
+  decorates_association :users
   def as_json *args
-    {
-      id: id,
-      name: name
-    }
+
+    if context[:index]
+
+      {
+        id: id,
+        name: name,
+        publisher: publisher,
+        year: year
+      }
+
+    elsif context[:show]
+
+      {
+        user_id: user_id,
+        name: name,
+        publisher: publisher,
+        year: year
+      }
+    end
   end
 end
