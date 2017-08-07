d7519d1f522f84377f6e12d6e5b7a99d753e1392
Alexbaboshyn <Alexbaboshyn@gmail.com>
2017-08-08 00:30:48 +0300

    Tests for observers

---
+++ spec/observers/book_observer_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe BookObserver, type: :observer do
+  let(:book) { stub_model Book}
+
+  subject { described_class.send(:new) }
+
+  describe '#after_commit_on_create' do
+    before do
+      expect(BookJob).to receive(:set).with(wait: 5.seconds) do
+        double.tap { |a| expect(a).to receive(:perform_later).with(book) }
+      end
+    end
+
+    it { expect { subject.after_commit_on_create(book) }.to_not raise_error }
+  end
+end
---
+++ spec/observers/user_observer_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe UserObserver, type: :observer do
+  let(:user) { stub_model User}
+
+  subject { described_class.send(:new) }
+
+  describe '#after_commit_on_create' do
+    before do
+      expect(UserMailer).to receive(:welcome).with(user) do
+        double.tap { |a| expect(a).to receive(:deliver_later) }
+      end
+    end
+
+    it { expect { subject.after_commit_on_create(user) }.to_not raise_error }
+  end
+end
